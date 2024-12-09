435fcf9b6afbd8341108a0230fb119da5e9495d9
GuilhermeSantana <guissantana105@gmail.com>
2024-12-09 20:37:02 -0300

    configuração de segurança

--- leilao/android/app/src/main/kotlin/com/example/leilao/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.leilao
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Importa o pacote dotenv
 import 'package:leilao/pages/login/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Sempre antes de inicializações
 
+  // Carrega o arquivo .env
+  await dotenv.load(fileName: ".env");
+
   if (kIsWeb) {
-    const firebaseConfig = FirebaseOptions(
-      apiKey: "AIzaSyDPOVs8jPIGiT7BZBnXG1t6S3z6vRe7cBo",
-      authDomain: "mercadinho-5d656.firebaseapp.com",
-      projectId: "mercadinho-5d656",
-      storageBucket: "mercadinho-5d656.firebasestorage.app",
-      messagingSenderId: "576728877247",
-      appId: "1:576728877247:web:4b0650dec50c6706ab9e0a",
-      measurementId: "G-BHZGM4W5CE",
+    // Carrega as configurações do Firebase a partir do .env
+    final firebaseConfig = FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      authDomain: dotenv.env['AUTH_DOMAIN']!,
+      projectId: dotenv.env['PROJECT_ID']!,
+      storageBucket: dotenv.env['STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['APP_ID']!,
+      measurementId: dotenv.env['MEASUREMENT_ID']!,
     );
     await Firebase.initializeApp(options: firebaseConfig);
   } else {
